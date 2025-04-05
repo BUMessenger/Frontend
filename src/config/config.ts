63c26d92c53a5a17e03ff39913cdaa036c21a26c
solula <86023773+solula@users.noreply.github.com>
2025-04-05 23:49:15 +0300

    #5 - Изменены конфиг и добавлен .docker.env

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,11 +1,9 @@
 export interface Config {
     basePath: string;
     apiHost: string;
-    graphqlEndpoint: string;
 }
 
 export const cfg: Config = {
     basePath: import.meta.env.VITE_BASE_PATH!,
     apiHost: import.meta.env.VITE_API_HOST!,
-    graphqlEndpoint: import.meta.env.VITE_GRAPHQL_ENDPOINT!,
 };
