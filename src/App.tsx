215d9b0d9e3487217e62a48b4f3c6b952a003e34
solula <86023773+solula@users.noreply.github.com>
2025-04-05 20:59:53 +0300

    #5 - Изменила структуру каталогов страниц

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import PageLayout from "src/components/page/PageLayout";
 import { cfg } from "src/config/config";
-import SignInPage from "src/pages/AuthPageS/SignInPage/SignInPage";
+import SignInPage from "src/pages/AuthPageS/SignInPage";
 import NotFoundPage from "src/pages/NotFoundPage/NotFoundPage";
-import CheckEmailPage from "./pages/AuthPageS/CheckEmailPage/CheckEmailPage";
-import LoginPage from "./pages/AuthPageS/LoginPage/LoginPage";
-import UserPage from "./pages/AuthPageS/UserPage/UserPage";
+import CheckEmailPage from "./pages/AuthPageS/CheckEmailPage";
+import LoginPage from "./pages/AuthPageS/LoginPage";
+import UserPage from "./pages/AuthPageS/UserPage";
 
 export default function App() {
     return (
